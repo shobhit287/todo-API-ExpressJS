a410f92fabb25c57a4f2c9ea65cb39a8e754d781
aditiroygithub <aditi.roy@aksconsultingservices.com>
2025-01-06 23:11:02 +0530

    update cors

--- conifg/cors.js
+++ conifg/cors.js
@@ -20,7 +20,7 @@ const corsOptions = {
     }
   },
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
