dd72d0bfba42e568ff46639f3eb84bc9e6859079
aditiroygithub <aditi.roy@aksconsultingservices.com>
2025-01-06 22:43:08 +0530

    update constant

--- conifg/constant.js
+++ conifg/constant.js
@@ -1,1 +1,1 @@
-exports.allowedDomains = [process.env.CLIENT_URL, process.env.SERVER_URL];
+exports.allowedDomains = [process.env.CLIENT_URL, process.env.SERVER_URL,"https://todo-react-portal.vercel.app/"];
