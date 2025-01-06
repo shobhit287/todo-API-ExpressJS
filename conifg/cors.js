50832ae385a58f578f43d1888bb1c3298ba57fa8
aditiroygithub <aditi.roy@aksconsultingservices.com>
2025-01-06 22:49:17 +0530

    allowed all origins

--- conifg/cors.js
+++ conifg/cors.js
@@ -2,23 +2,24 @@ const cors = require("cors");
 const { allowedDomains } = require("./constant");
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    try {
-      if (allowedDomains.includes(origin)) {
-        callback(null, true);
-      } else {
-        let msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        callback(new Error(msg), false);
-      }
-    } catch (error) {
-      let msg =
-        "The CORS policy for this site does not " +
-        "allow access from the specified Origin.";
-      callback(new Error(msg), false);
-    }
-  },
+  // origin: function (origin, callback) {
+  //   try {
+  //     if (allowedDomains.includes(origin)) {
+  //       callback(null, true);
+  //     } else {
+  //       let msg =
+  //         "The CORS policy for this site does not " +
+  //         "allow access from the specified Origin.";
+  //       callback(new Error(msg), false);
+  //     }
+  //   } catch (error) {
+  //     let msg =
+  //       "The CORS policy for this site does not " +
+  //       "allow access from the specified Origin.";
+  //     callback(new Error(msg), false);
+  //   }
+  // },
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type"],
   credentials: true,
