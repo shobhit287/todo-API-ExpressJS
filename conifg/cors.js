8f49c35ea568208bd38ddbbd29f0a38c9453dd2e
aditiroygithub <aditi.roy@aksconsultingservices.com>
2025-01-06 23:10:34 +0530

    update cors for render portal

--- conifg/cors.js
+++ conifg/cors.js
@@ -2,24 +2,23 @@ const cors = require("cors");
 const { allowedDomains } = require("./constant");
 
 const corsOptions = {
-  // origin: function (origin, callback) {
-  //   try {
-  //     if (allowedDomains.includes(origin)) {
-  //       callback(null, true);
-  //     } else {
-  //       let msg =
-  //         "The CORS policy for this site does not " +
-  //         "allow access from the specified Origin.";
-  //       callback(new Error(msg), false);
-  //     }
-  //   } catch (error) {
-  //     let msg =
-  //       "The CORS policy for this site does not " +
-  //       "allow access from the specified Origin.";
-  //     callback(new Error(msg), false);
-  //   }
-  // },
-  origin: "*",
+  origin: function (origin, callback) {
+    try {
+      if (allowedDomains.includes(origin)) {
+        callback(null, true);
+      } else {
+        let msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        callback(new Error(msg), false);
+      }
+    } catch (error) {
+      let msg =
+        "The CORS policy for this site does not " +
+        "allow access from the specified Origin.";
+      callback(new Error(msg), false);
+    }
+  },
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type"],
   credentials: true,
